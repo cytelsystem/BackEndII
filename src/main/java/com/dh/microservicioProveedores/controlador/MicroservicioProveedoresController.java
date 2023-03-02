27781351468dde39d296fb10b0c2c8a135e88993
Hector Javier Moreno <cytelsystem@gmail.com>
2023-03-02 17:35:22 -0500

    adicionando cambios a microservicioProveedores

--- src/main/java/com/dh/microservicioProveedores/controlador/MicroservicioProveedoresController.java
+++ src/main/java/com/dh/microservicioProveedores/controlador/MicroservicioProveedoresController.java
@@ -2,6 +2,8 @@ package com.dh.microservicioProveedores.controlador;
 
 import com.dh.microservicioProveedores.service.microservicioProveedoresService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class MicroservicioProveedoresController {
+
+    @Value("${idRandom}")
+    private String port;
 
     @Autowired
     private microservicioProveedoresService servicedatos;
@@ -25,6 +30,11 @@ public class MicroservicioProveedoresController {
     @GetMapping("/saludar")
     public String getSaludar() {
         return "Hola como estas";
+    }
+
+    @GetMapping("/subscription/find")
+    public ResponseEntity<String> find() {
+        return ResponseEntity.ok(port);
     }
 
 }
